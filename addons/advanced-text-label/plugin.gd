b95608e9f12e270d8fc5268cb9bc4cc307ea908d
Jeremi Biernacki <jeremi360@gmail.com>
2021-10-21 17:19:44 +0200

    full mode, try to made toggle works

--- addons/advanced-text-label/plugin.gd
+++ addons/advanced-text-label/plugin.gd
@@ -4,6 +4,8 @@ extends EditorPlugin
 var atl_inspector
 var markup_text_editor_button := ToolButton.new()
 var markup_text_editor
+var editor_parent : Control
+var button_parent : Control
 
 func _enter_tree():
 	atl_inspector = preload("inspector/AdvancedTextLabelInspector.gd")
@@ -14,15 +16,25 @@ func _enter_tree():
 	markup_text_editor = markup_text_editor.instance()
 	markup_text_editor.visible = false
 	markup_text_editor.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	get_editor_interface().get_editor_viewport().add_child(markup_text_editor)
+	markup_text_editor.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	editor_parent = get_editor_interface().get_editor_viewport()
+	editor_parent.add_child(markup_text_editor)
 	
 	markup_text_editor_button.text = "Markup Text Editor"
 	markup_text_editor_button.icon = preload("MarkupTextEditor/MarkupTextEditor.svg")
 	markup_text_editor_button.toggle_mode = true
+	markup_text_editor_button.pressed = false
+	markup_text_editor_button.action_mode = ToolButton.ACTION_MODE_BUTTON_RELEASE
+
+	# hack to add the button to the editor modes tabs
 	add_control_to_container(CONTAINER_TOOLBAR, markup_text_editor_button)
-	var button_parent = markup_text_editor_button.get_parent()
+	button_parent = markup_text_editor_button.get_parent()
 	button_parent.remove_child(markup_text_editor_button)
 	button_parent = button_parent.get_child(2)
+
+	for b in button_parent.get_children():
+		b.connect("pressed", self, "_on_toggle", [false])
+
 	markup_text_editor_button.connect("toggled", self, "_on_toggle")
 	button_parent.add_child(markup_text_editor_button)
 
@@ -31,5 +43,21 @@ func _exit_tree():
 	markup_text_editor.queue_free()
 	markup_text_editor_button.queue_free()
 
+func hide_current_editor():
+	for ch in editor_parent.get_children():
+		if ch.visible:
+			ch.visible = false
+			return
+
+func unpressed_current_button():
+	for ch in button_parent.get_children():
+		if ch.pressed:
+			ch.pressed = false
+			return
+
 func _on_toggle(toggled:bool):
+	if toggled:
+		hide_current_editor()
+		unpressed_current_button()
+		
 	markup_text_editor.visible = toggled
