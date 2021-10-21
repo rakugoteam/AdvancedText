7d52c2d611f3531b97c7fd73f68bd14572c19bc9
Jeremi Biernacki <jeremi360@gmail.com>
2021-10-21 20:52:02 +0200

    make Help popup work

--- addons/advanced-text-label/MarkupTextEditor/MarkupTextEditor.gd
+++ addons/advanced-text-label/MarkupTextEditor/MarkupTextEditor.gd
@@ -5,11 +5,16 @@ export var markups_options_nodepath : NodePath
 export var edit_tabs_nodepath : NodePath
 export var preview_tabs_nodepath : NodePath
 export var preview_toggle_nodepath : NodePath
+export var help_button_nodepath : NodePath
+export var help_tabs_nodepath : NodePath
 
 onready var markups_options : OptionButton = get_node(markups_options_nodepath)
 onready var edit_tabs : TabContainer = get_node(edit_tabs_nodepath)
 onready var preview_tabs : TabContainer = get_node(preview_tabs_nodepath)
 onready var preview_toggle : CheckButton = get_node(preview_toggle_nodepath)
+onready var help_button : Button = get_node(help_button_nodepath)
+onready var help_tabs : TabContainer = get_node(help_tabs_nodepath)
+onready var help_popup : WindowDialog = help_tabs.get_parent()
 
 var markup_id := 0
 var text: = ""
@@ -18,6 +23,7 @@ func _ready():
 	update_text_preview(get_current_edit_tab())
 	markups_options.connect("item_selected", self, "_on_option_selected")
 	preview_toggle.connect("toggled", self, "_on_toggle")
+	help_button.connect("pressed", self, "_on_help_button_pressed")
 	
 	for ch in edit_tabs.get_children():
 		ch.connect("text_changed", self, "update_text_preview", [ch, true])
@@ -48,10 +54,14 @@ func update_text_preview(caller:TextEdit, change_text := true):
 
 func _on_option_selected(id: int):
 	if id != markup_id:
-		
 		edit_tabs.current_tab = id
 		preview_tabs.current_tab = id
+		help_tabs.current_tab = id
 		markup_id = id
 
 		var current := get_current_edit_tab()
 		update_text_preview(current, text.empty())
+
+func _on_help_button_pressed():
+	help_popup.window_title = markups_options.get_item_text(markup_id) + " Help"
+	help_popup.popup_centered(Vector2(500, 500))
