2f25dd85dfdbad5ec814ae0fbacb067dad80b60b
Jeremi Biernacki <jeremi360@gmail.com>
2021-11-08 21:30:56 +0100

    Add AdvancedTextButton Example

---
+++ addons/advanced-text/examples/AdvancedTextButton/AdvancedTextButton.gd
@@ -0,0 +1,58 @@
+# tool
+extends Button
+class_name AdvancedTextButton
+
+onready var adv_label := get_node("AdvancedTextLabel")
+
+func _ready():
+	adv_label.connect("resized", self, "_on_button_resized")
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
+	connect("pressed", self, "_on_pressed")
+	connect("toggled", self, "_on_toggled")
+
+func _set_label_color(color_name:String):
+	var color_path := "Button/colors/" + color_name
+	adv_label.modulate = theme.get(color_path)
+
+func _set(property:String, value)->bool:
+	if  property in get_property_list():
+		set(property, value)
+	else:
+		return false
+
+	match property:
+		"disabled":
+			if value:
+				_set_label_color("font_color_disabled")
+			else:
+				_set_label_color("font_color")
+				
+	return true
+
+func _on_button_resized():
+	adv_label.rect_size = rect_size
+
+func is_toggled():
+	return pressed and toggle_mode
+
+func _on_mouse_entered():
+	if not (disabled or is_toggled()):
+		_set_label_color("font_color_hover")
+
+func _on_mouse_exited():
+	if not (disabled or is_toggled()):
+		_set_label_color("font_color")
+
+func _on_pressed():
+	if not disabled:
+		_set_label_color("font_color_pressed")
+
+func _on_toggled(value:bool):
+	if not disabled:
+		if value:
+			_set_label_color("font_color_pressed")
+		else:
+			_set_label_color("font_color")
+
+
