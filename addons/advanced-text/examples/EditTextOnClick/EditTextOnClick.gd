8aa4f49bd4cf5a6b9bb02c272f78b39fa5c957f6
Jeremi Biernacki <jeremi360@gmail.com>
2021-11-11 08:58:54 +0100

    add EditTextOnClick example

--- addons/advanced-text/examples/EditTextOnClick/EditTextOnClick.gd
+++ addons/advanced-text/examples/EditTextOnClick/EditTextOnClick.gd
@@ -1,19 +1,25 @@
 extends TabContainer
 
-var text := "[center][shake rate=5 level=10]**Clik to edit me**[/shake][/center]"
+export(String, MULTILINE) var text := "[center][shake rate=5 level=10]**Clik to edit me**[/shake][/center]"
 onready var label := $AdvancedTextButton/AdvancedTextLabel
+onready var save_button := $MarkupEdit/Button
+onready var m_edit := $MarkupEdit
+onready var button := $AdvancedTextButton
 
 func _ready():
 	label.markup_text = text
-	$MarkupEdit.text = text
-	$AdvancedTextButton.connect("pressed", self, "set", ["current_tab", 1])
+	m_edit.text = text
+	button.connect("pressed", self, "_on_button_pressed")
+	save_button.connect("pressed", self, "_on_save_button_pressed")
+
+func _on_button_pressed():
+	current_tab = 1
+
+func _on_save_button_pressed():
+	label.markup_text = m_edit.text
+	current_tab = 0
 
 func _process(delta):
-	if current_tab == 1:
-		if Input.is_key_pressed(KEY_ENTER):
-			text = $MarkupEdit.text
-			label.markup_text = text
-			current_tab = 0
-			
+	if m_edit.visible:
 		if Input.is_key_pressed(KEY_ESCAPE):
 			current_tab = 0
