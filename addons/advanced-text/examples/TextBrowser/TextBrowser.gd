2f8ba341fbc98baa8cd7f79e56c7cc95d5a7c181
Jeremi Biernacki <jeremi360@gmail.com>
2021-11-12 16:50:00 +0100

    add tabs switcher and opeing text files

--- addons/advanced-text/examples/TextBrowser/TextBrowser.gd
+++ addons/advanced-text/examples/TextBrowser/TextBrowser.gd
@@ -1,5 +1,6 @@
 extends Panel
 
+export(String, FILE, "*.md, *.rpy, *.txt") var home_text_file := ""
 export var tab_button_scene : PackedScene
 export var text_view_scene : PackedScene
 
@@ -7,10 +8,40 @@ onready var tabs : TabContainer = $HSplitContainer/TabContainer
 onready var tabs_box : VBoxContainer = $HSplitContainer/ScrollContainer/VBoxContainer
 
 var current_text_view : AdvancedTextLabel
+var tabs_group := ButtonGroup.new()
+var tabs_dir := {}
 
 func _ready():
-	current_text_view = tabs.get_current_tab_control()
-	current_text_view.connect("meta_clicked", self, "_on_meta_clicked")
+	_open_file(home_text_file)
+
+func _open_file(file:String) -> void:
+	var new_text_view = text_view_scene.instance()
+	new_text_view.markup_text_file = file
+	new_text_view.connect("meta_clicked", self, "_on_meta_clicked")
+	tabs.add_child(new_text_view)
+	tabs.current_tab = tabs.get_tab_count() - 1
+
+	var new_tab_button = tab_button_scene.instance()
+	var file_name = file.get_file()
+	var button : Button = new_tab_button.get_node("FileButton")
+	button.text = file_name
+	button.toggle_mode = true
+	button.pressed = true
+	button.group = tabs_group
+
+	var close_button : Button = new_tab_button.get_node("CloseButton")
+	close_button.connect("pressed", self, "_on_tab_closed", [new_tab_button])
+	close_button.text = ""
+	close_button.icon = get_icon("Close", "EditorIcons")
+	tabs_box.add_child(new_tab_button)
+
+	tabs_dir[new_tab_button] = new_text_view
 
 func _on_meta_clicked(meta:String):
-	pass
+	if meta.begins_with("res://"):
+		_open_file(meta)
+
+func _on_tab_closed(caller:Node):
+	var text_view = tabs_dir[caller]
+	tabs.remove_child(text_view)
+	tabs_box.remove_child(caller)
