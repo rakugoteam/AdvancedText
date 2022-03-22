cdafc2eb2cb696abcb6213464847c2996eb36249
Jeremi Biernacki <jeremi360@gmail.com>
2022-03-22 15:31:30 +0100

    make emoji and icons buttons works again in new ToolBar

--- addons/advanced-text/MarkupTextEditor/ToolBar.gd
+++ addons/advanced-text/MarkupTextEditor/ToolBar.gd
@@ -1,6 +1,12 @@
 tool
 extends Control
 
+var EmojisImport
+var emojis_gd
+
+var IconsImport
+var icons_gd
+
 export var AdvancedTextLabelIcon : Texture
 export var CodeEditIcon : Texture
 export var MarkdownIcon : Texture
@@ -10,10 +16,12 @@ export var BBCodeIcon : Texture
 onready var preview_toggle : Button = $HBoxContainer/HBoxContainer/PreviewToggle
 onready var help_button : Button = $HBoxContainer/HBoxContainer/HelpButton
 onready var selected_node_toggle : Button = $HBoxContainer/EditSelectedNode
-onready var files_toggle : Button = $HBoxContainer/EditSelectedNode
+onready var files_toggle : Button = $HBoxContainer/EditSelectedFile
 onready var help_popup : Popup = $PopupHelp
 onready var markup_switch : OptionButton = $HBoxContainer/CenterContainer/HBoxContainer/MarkupOption
 onready var preview_switch : OptionButton = $HBoxContainer/HBoxContainer/PreviewOption
+onready var emoji_button : Button = $HBoxContainer/HBoxContainer/EmojisButton
+onready var icon_button : Button = $HBoxContainer/HBoxContainer/IconsButton
 
 signal preview_toggle
 signal help_pressed
@@ -22,6 +30,8 @@ signal selected_markup(mode)
 signal selected_preview(mode)
 
 func _ready():
+	import_emojis()
+	import_mat_icons()
 	preview_toggle.connect("toggled", self, "emit_signal", ["preview_toggle"])
 	preview_toggle.icon = get_icon("RichTextEffect", "EditorIcons")
 
@@ -50,3 +60,29 @@ func _on_preview_changed(mode):
 func _on_markup_changed(mode):
 	var txt_mode = markup_switch.get_item_text(mode)
 	emit_signal("selected_markup", txt_mode)
+
+func import_emojis():
+	EmojisImport = preload("../emojis_import.gd")
+	EmojisImport = EmojisImport.new()
+
+	if EmojisImport.is_plugin_enabled():
+		var emoji_panel : Popup = EmojisImport.get_emoji_panel()
+		emoji_panel.visible = false
+		add_child(emoji_panel)
+		emoji_button.connect("pressed", emoji_panel, "popup_centered", [Vector2(450, 400)])
+		emoji_button.show()
+	else:
+		EmojisImport.free()
+
+func import_mat_icons():
+	IconsImport = preload("../material_icons_import.gd")
+	IconsImport = IconsImport.new()
+
+	if IconsImport.is_plugin_enabled():
+		var icon_panel : Popup = IconsImport.get_icons_panel()
+		icon_panel.visible = false
+		add_child(icon_panel)
+		icon_button.connect("pressed", icon_panel, "popup_centered", [Vector2(450, 400)])
+		icon_button.show()
+	else:
+		IconsImport.free()
