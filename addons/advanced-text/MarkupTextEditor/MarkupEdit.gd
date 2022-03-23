d5e23f05d66526e0a07c1e9dd38d33fb5a7a602f
Jeremi Biernacki <jeremi360@gmail.com>
2022-03-23 22:32:58 +0100

    MarkupEdit script to help switching markups

--- addons/advanced-text/MarkupTextEditor/MarkupEdit.gd
+++ addons/advanced-text/MarkupTextEditor/MarkupEdit.gd
@@ -1,14 +1,34 @@
+tool
 extends CodeEdit
 
-export var configs_sets := {
-		"markdown": ["res://addons/advanced-text/highlights/bbcode.json"],
-		"bbcode": ["res://addons/advanced-text/highlights/bbcode.json"],
-		"renpy": ["res://addons/advanced-text/highlights/renpy.json"],
-		"json": ["res://addons/advanced-text/highlights/json.json"],
-		"gdscript": ["res://addons/advanced-text/highlights/gdscript.json"],
+export var p_path : NodePath 
+export (NodePath) onready var parent = get_node(parent)
+export var configs_dict := {
+		"markdown": "res://addons/advanced-text/highlights/bbcode.json",
+		"bbcode": "res://addons/advanced-text/highlights/bbcode.json",
+		"renpy": "res://addons/advanced-text/highlights/renpy.json",
+		"json": "res://addons/advanced-text/highlights/json.json",
+		"gdscript": "res://addons/advanced-text/highlights/gdscript.json",
 	}
 
-func change_markup(markup:String):
+func _ready():
+	parent.connect("ready", self, "_on_parent_ready")
+
+func _on_parent_ready():
+	parent.toolbar.connect("selected_markup", self, "_on_markup_selected")
+
+func _on_markup_selected(markup:String):
+	var lang = [markup.to_lower()]
+	change_configs(lang)
+
+func change_configs(langues: Array) -> void:
 	clear_colors()
-	configs = configs_sets[markup]
+	
+	configs = []
+	for langue in langues:
+		configs.append(configs_dict[langue])
+
 	_add_keywords_highlighting()
+
+
+
