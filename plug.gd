7774e7575888d7b8829ad8f801fefbf995d75c47
Jeremi Biernacki <jeremi360@gmail.com>
2022-03-26 10:45:38 +0100

    update instructions for installing emojis and icons

--- plug.gd
+++ plug.gd
@@ -3,7 +3,13 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
     # Declare your plugins in here with plug(src, args)
     # By default, only "addons/" directory will be installed
+    
+    # dependencies
     plug("rakugoteam/project-settings-helpers")
-    plug("rakugoteam/Emojis-For-Godot")
-    plug("rakugoteam/Godot-Material-Icons")
+
+    # optional supported plugins
+    plug("rakugoteam/Emojis-For-Godot", {"include": [".import/"]})
+    plug("rakugoteam/Godot-Material-Icons", {"include": [".import/"]})
+    
+    # optional plugins to help with development
     plug("Xrayez/godot-editor-icons-previewer")
